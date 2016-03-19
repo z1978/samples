f296f430a67f28c391f76accedeca8f3c9f9aff7
znb <znb78@yahoo.co.jp>
2016-03-19 10:38:58 +0800

    user.js

---
+++ user.js/test01.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        Hello World !
+// @namespace   http://www.m-bsys.com/
+// @description 「Hello World !」を表示するだけのサンプルです。「http://www.m-bsys.com/」の「code」以外のページを開いた場合にのみ動作します。
+// @include     http://www.m-bsys.com/*
+// @exclude     http://www.m-bsys.com/code/*
+// ==/UserScript==
+
+alert("Hello world!");
