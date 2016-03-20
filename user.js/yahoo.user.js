6554fa5ebf834feed1bcb3260bccba610a34a9f5
znb <znb78@yahoo.co.jp>
2016-03-20 17:47:04 +0800

    user.js

---
+++ user.js/yahoo.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        test user js
+// @namespace   http://www.yahoo.co.jp/
+// @description 「Hello Yahoo !」を表示するだけのサンプルです。「http://www.yahoo.co.jp/」の「code」以外のページを開いた場合にのみ動作します。
+// @include     http://www.yahoo.co.jp/*
+// @exclude     http://www.yahoo.co.jp/code/*
+// ==/UserScript==
+
+alert("Hello Yahoo!");
